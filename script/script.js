7df61a42c37096f74a86f635eedf2bafe0b1e8f2
Amal <olympqwe@gmail.com>
2023-05-28 13:01:07 +0500

    Chellenge project end

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let minus = document.querySelector('.main__minus')
+let count = document.querySelector('.main__kol')
+let plus = document.querySelector('.main__plus')
+
+plus.addEventListener('click',function(){
+    count++
+})
